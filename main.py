e5683c8b13d8be94b9c0277a1900e20c4a0fae94
Jessy Randriamizara <henintsoarandriam@gmail.com>
2025-07-31 09:02:24 +0300

    added put request for /posts

--- main.py
+++ main.py
@@ -32,14 +32,28 @@ def welcome():
         html_content = file.read()
     return Response(content=html_content, status_code=200, media_type="text/html")
 
+@app.get("/posts")
+def list_objects():
+    return {"posts": serialized_objects_list()}
 @app.post("/posts")
 def new_objects(object_payload: List[ObjectModel]):
     objects_list.extend(object_payload)
     return JSONResponse({"objects": serialized_objects_list()}, status_code=201)
 
-@app.get("/posts")
-def list_objects():
-    return {"posts": serialized_objects_list()}
+@app.put("/posts")
+def update_or_create_objects(object_payload: List[ObjectModel]):
+    global objects_list
+
+    for new_object in object_payload:
+        found = False
+        for i, existing_object in enumerate(objects_list):
+            if new_object.title == existing_object.title:
+                objects_list[i] = new_object
+                found = True
+                break
+        if not found:
+            objects_list.append(new_object)
+    return {"events": serialized_objects_list()}
 
 @app.get("/{full_path:path}")
 def catch_all(full_path: str):
