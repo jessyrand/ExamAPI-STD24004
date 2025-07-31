2de080bddafe9d353cf10194171db0681f77d281
Jessy Randriamizara <henintsoarandriam@gmail.com>
2025-07-31 08:36:33 +0300

    added 404 not found page and request

--- main.py
+++ main.py
@@ -16,5 +16,9 @@ def welcome():
     return Response(content=html_content, status_code=200, media_type="text/html")
 
 
-
+@app.get("/{full_path:path}")
+def catch_all(full_path: str):
+    with open("not_found.html", "r", encoding="utf-8") as file:
+        html_content = file.read()
+    return Response(content=html_content, status_code=404, media_type="text/html")
 
