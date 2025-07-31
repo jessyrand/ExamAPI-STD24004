31b7e611b55885b0a30bd93bc75e66438e17bd91
Jessy Randriamizara <henintsoarandriam@gmail.com>
2025-07-31 08:33:37 +0300

    added get /home request

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette.requests import Request
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, Response
 
 app = FastAPI()
 
 @app.get("/ping")
-def read_root():
+def pingpong():
     return "pong"
+
+@app.get("/home")
+def welcome():
+    with open("welcome.html", "r", encoding="utf-8") as file:
+        html_content = file.read()
+    return Response(content=html_content, status_code=200, media_type="text/html")
+
 
 
 
